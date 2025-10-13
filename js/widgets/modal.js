a74dd87ddf2ea468962a9edadec103a68b44d1c9
Santiago Sepúlveda <60012979+Sepu2002@users.noreply.github.com>
2025-10-13 13:00:33 -0600

    Update modal.js

--- js/widgets/modal.js
+++ js/widgets/modal.js
@@ -55,7 +55,7 @@ export function showModal(title, content, autoCloseDelay = 0, inputSelector = nu
                     '~ ! @ # $ % ^ & * ( ) _ + {bksp}',
                     '{tab} Q W E R T Y U I O P { } |',
                     '{lock} A S D F G H J K L : " {enter}',
-                    '@gmail Z X C V B N M < > ? {shift}',
+                    '{shift} Z X C V B N M < > ? {shift}',
                     '.com @ {space}'
                 ]
             },
